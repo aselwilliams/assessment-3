99dd787232d59b96288693bf5f89a139f5e1c191
Asel Williams <aselwilliams22@gmail.com>
2022-11-19 00:17:39 -0600

    formatted the code

--- about.js
+++ about.js
@@ -1,14 +1,14 @@
 console.log("hello world");
-let form = document.querySelector('#contact');
-let catImg = document.querySelector('#cat-img');
+let form = document.querySelector("#contact");
+let catImg = document.querySelector("#cat-img");
 
 function handleSubmit(evt) {
-	evt.preventDefault();
-	
-	alert('The form submitted successfully.')
-	document.querySelector('#your-name').value = ''
-	document.querySelector('#message').value = ''
+  evt.preventDefault();
+
+  alert("The form submitted successfully.");
+  document.querySelector("#your-name").value = "";
+  document.querySelector("#message").value = "";
 }
 
-form.addEventListener('submit', handleSubmit);
-catImg.addEventListener('mouseover', ()=> alert("You're awesome!"))
+form.addEventListener("submit", handleSubmit);
+catImg.addEventListener("mouseover", () => alert("You're awesome!"));
